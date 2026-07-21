a9c1dd6709906e96ce907cbd624159c9be95f8fb
Martin Morales <martinandresmm10@gmail.com>
2026-07-20 20:15:41 -0600

    Estructura completa de url-mia-2026

---
+++ semana-1/README.md.txt
@@ -0,0 +1,7 @@
+# Laboratorio No. 1 - Git
+Nombre: Martin Morales Carné: 1217025
+Curso: Métodos e Implementación de Archivos
+Fecha: viernes 10 de julio del 2026
+Sección: 2
+Actividad: Laboratorio No. 1
+
---
+++ semana-2/Laboratorio_2_CSV_JSON/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string csv = "estudiantes.csv";
+        string json = "estudiantes.json";
+
+        if (!File.Exists(csv))
+        {
+            Console.WriteLine($"El archivo {csv} no existe.");
+            return;
+        }
+
+        string[] lineas = File.ReadAllLines(csv);
+        List<Estudiante> listaEstudiantes = new List<Estudiante>();
+
+        for (int i = 1; i < lineas.Length; i++)
+        {
+            string linea = lineas[i];
+            if (string.IsNullOrWhiteSpace(linea)) continue;
+
+            string[] datos = linea.Split(',');
+
+            if (datos.Length >= 3)
+            {
+                Estudiante estudiante = new Estudiante
+                {
+                    Id = int.Parse(datos[0].Trim()),
+                    Nombre = datos[1].Trim(),
+                    Carrera = datos[2].Trim()
+                };
+
+                listaEstudiantes.Add(estudiante);
+            }
+        }
+
+        foreach (var est in listaEstudiantes)
+        {
+            Console.WriteLine($"{est.Id} - {est.Nombre} - {est.Carrera}");
+        }
+
+        var opcionesJson = new JsonSerializerOptions { WriteIndented = true };
+        string jsonResultado = JsonSerializer.Serialize(listaEstudiantes, opcionesJson);
+
+        File.WriteAllText(json, jsonResultado);
+
+        Console.WriteLine("Archivo estudiantes.json creado correctamente.");
+    }
+}
